9a9bdfced822dfb61bf22e5c389bf3318951333e
Brian Shewan <brian.shewan@nscc.ca>
2025-01-08 13:56:14 -0400

    finished react example

--- sample-react/src/index.jsx
+++ sample-react/src/index.jsx
@@ -1,4 +1,6 @@
 import { createRoot } from "react-dom/client";
+import './site.css';
+import robot from './robot.png';
 
 function h1_onclick() {
   const div = document.getElementById('content');
@@ -19,7 +21,7 @@ root.render(
       Aspernatur at consequuntur esse provident quae unde voluptates?
       <em>Consequatur dolorum excepturi fugiat ipsum mollitia necessitatibus nisi,
         odio rem, sint soluta tempora veniam.</em></p>
-    <img src="robot.png" width="200" alt="robot"/>
+    <img src={robot} width="200" alt="robot"/>
   </div>
   </>
 );
