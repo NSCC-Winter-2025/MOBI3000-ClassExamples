9581ad54d86b5d9316b2856c262e1a7cb5b4f17a
Brian Shewan <brian.shewan@nscc.ca>
2025-01-30 10:17:11 -0400

    completed demo version

--- expo-minimum/App.js
+++ expo-minimum/App.js
@@ -1,12 +1,15 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {useFonts} from "expo-font";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, Pressable, StyleSheet, Text, View} from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
+import {Foundation} from "@expo/vector-icons";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [text, setText] = useState('Hello World!');
 
   const [loaded, error] = useFonts({
     'Designer': require('./assets/fonts/Designer.otf'),
@@ -20,9 +23,24 @@ export default function App() {
 
   if (!error && !loaded) return null;
 
+  const handleButtonPress = () => {
+    setText('You pressed the button!');
+  }
+
+  const handleTextPress = () => {
+    setText("That's FAST!");
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>FAST & FURIOUS!</Text>
+      <Pressable onPress={handleTextPress}>
+        <Text style={styles.text}>FAST & FURIOUS!</Text>
+      </Pressable>
+      <View style={styles.view}>
+        <Text style={{color: 'white', fontSize: 25}}>{text}</Text>
+        <Foundation name="bitcoin" size={75} color="white" />
+      </View>
+      <Button title="Press ME!" onPress={handleButtonPress} />
       <StatusBar style="auto" />
     </View>
   );
@@ -39,5 +57,15 @@ const styles = StyleSheet.create({
     fontFamily: 'Designer',
     color: 'red',
     fontSize: 50
+  },
+  view: {
+    backgroundColor: '#00f',
+    height: 200,
+    width: 200,
+    borderWidth: 5,
+    borderColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
   }
 });
